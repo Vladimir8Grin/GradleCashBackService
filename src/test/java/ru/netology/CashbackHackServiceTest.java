12c3b2cdc900ee27b34715b14a1eb841f8f13039
“Vladimir_Grin” <mortywallet@gmail.com>
2024-02-15 21:30:26 +0300

    testng

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,8 +1,31 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class CashbackHackServiceTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemainWithLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int result = service.remain(amount);
+        Assert.assertEquals(result, 500);
+    }
 
+    @Test
+    public void testRemainWithExactlyBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int result = service.remain(amount);
+        Assert.assertEquals(result, 0);
+    }
+
+    @Test
+    public void testRemainWithGreaterThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int result = service.remain(amount);
+        Assert.assertEquals(result, 500);
+    }
 }
